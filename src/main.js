f68a76911acc42425ef8d38c633e95fe89c9139a
gliese-backup <glieseb+1@proton.me>
2024-10-27 09:14:11 +0000

    render image on change

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-/* API Endpoints
- * breeds list: https://dog.ceo/api/breeds/list/all
- * get single breed image: https://dog.ceo/api/breed/{breed}/images/random
- */
-
-// DOM Selection
+// === DOM Selection
 const selectEl = document.querySelector("#dog-breeds");
+const imageEl = document.querySelector("#dog-image");
 
+// === Fetch
 // This function returns [breeds]
 function getDogsBreed() {
   return fetch(`https://dog.ceo/api/breeds/list/all`)
@@ -21,8 +18,7 @@ function getDogImage(breed) {
     .catch((err) => console.log(err));
 }
 
-getDogImage("husky");
-
+// === Render
 // Renders the <option>
 function renderOptions() {
   getDogsBreed().then((breeds) => {
@@ -36,3 +32,9 @@ function renderOptions() {
 }
 
 renderOptions();
+
+selectEl.addEventListener("change", (e) => {
+  const selectedValue = e.target.value;
+
+  getDogImage(selectedValue).then((link) => (imageEl.src = link));
+});
